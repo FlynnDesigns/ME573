f383ff6e4b0cf58de123e867cb0727108b4f030b
Nathan Flynn <nflynn4@wisc.edu>
2022-12-07 10:00:45 -0600

    Changed indexing to all I and J

--- FinalProject/part1.m
+++ FinalProject/part1.m
@@ -8,49 +8,55 @@ format long;
 dX = 0.05;
 dY = 0.05; 
 L = 1;
-gamma = 1;
+gamma = 0;
 nu = 0.005;
-C = 0.0001;
-tFinal  = 0.02;
+C = 0.01;
+tFinal  = 0.01;
 
 % Calcluated inputs
 dT = C * min(1/4 * dX^2/nu);
 time = 0:dT:tFinal;
 
 % Initializing nodes and applying initial conditions
-[u,v,xu,yu,xv,yv,x_p,y_p,nXu,nYu,nXv,nYv] = generateNodes(dX, dY, L);
+[u,v,xu,yu,xv,yv,x_p,y_p,I,J] = generateNodes(dX, dY, L);
 
 % Allocating arrays
 KE = zeros(1, length(time));
 KE(1) = 0;
-vCenter = zeros(nYv-1, nYv-1);
-uCenter = zeros(nXu-1,nXu-1);
+vCenter = zeros(length(2:I-1), length(2:J-1));
+uCenter = zeros(length(2:I-1), length(2:J-1));
 
 % Solving u and v
 for i = 1:length(0:dT:tFinal)
-    % Applying boundary conditions
+    %% Applying boundary conditions to the velocity field
+    % Left boundary
     u(1,:) = 0;
-    v(1,:) = 0;
-    u(nXu,:) = 0;
-    v(nXu+1,:) = 0;
+    v(1,:) = -v(2,:);
+    % Right boundary
+    u(I,:) = 0;
+    v(I+1,:) = -v(I,:);
+    % Bottom boundary
+    u(:,1) = -u(:,2);
     v(:,1) = 0;
-    v(:,nYv) = 0;
+    % Top boundary
+    u(:,J+1) = 0;
+    U = (u(:,J) + u(:,J+1))/2;
+    u(:,J+1) = 2*U-u(:,J);
+    v(:,J) = 0;
    
-    % Solving for u and v
-    [u, v] = solveUV(u, v, dX, dY, dT, nXu, nYu, nXv, nYv, 1, 1);
+%     % Solving for u and v
+%     [u, v] = solveUV(u,v,dX,dY,dT,I,J,gamma,nu);
 
     % Getting the centers of all of the u nodes
-    uCenter = zeros(nXu-1,nXu-1);
-    for j = 2:nYu-1
-        for  k = 2:nXu
+    for j = 2:J-1
+        for  k = 2:I-1
             uCenter(k-1,j-1) = (u(k-1,j) + u(k,j))/2;
         end
     end
 
     % Getting the centers of all of the v nodes 
-    vCenter = zeros(nYv-1, nYv-1);
-    for j = 2:nYv % running through y
-        for k = 2:nXv-1 % running through x
+    for j = 2:J-1 % running through y
+        for k = 2:I-1 % running through x
             vCenter(k-1,j-1) = (v(k,j-1) + v(k,j))/2;
         end
     end
@@ -61,8 +67,8 @@ for i = 1:length(0:dT:tFinal)
     % Calculating the kenetic energy (KE)
     KE(i) = KE(i) + sum(0.5*dX*dY*(uCenter.^2 + vCenter.^2),"All");
 end
-
-%% Plot 1
+%% Plots-------------------------------------------------------------------
+% Plot 1
 figure('units','normalized','position',[0.3,0.65,0.4,0.4])
 quiver(x_p,y_p,uCenter,vCenter,2)
 set(gca,'fontsize',26)
@@ -71,7 +77,7 @@ ylim([0,1])
 xlabel('x_p')
 ylabel('y_p')
 title('Velocity Field')
-%% Plot 2
+% Plot 2
 figure('units','normalized','position',[0.3,0,0.4,0.4])
 contour(x_p,y_p,umag,'ShowText','on')
 set(gca,'fontsize',26)
@@ -80,7 +86,7 @@ ylim([0,1])
 xlabel('x_p')
 ylabel('y_p')
 title('Velocity Magnitude')
-%% Plot 3
+% Plot 3
 figure('units','normalized','position',[0.65,0.01,0.5,0.5])
 plot(time,KE,'-')
 set(gca,'fontsize',26)
@@ -88,7 +94,7 @@ xlabel('time (s)')
 %legend('\nu=0.005','\nu=0.01','\nu=0.05')
 title('Total Kinetic Energy')
 %% Function to generate U nodes
-function [u,v,xu,yu,xv,yv,x_p,y_p,nXu,nYu,nXv,nYv] = generateNodes(dX, dY, L)
+function [u,v,xu,yu,xv,yv,x_p,y_p,I,J] = generateNodes(dX, dY, L)
 % Nodes in x
 xu = 0:dX:L;
 xv = -dX/2:dX:L+dX/2;
@@ -110,38 +116,44 @@ v = v';
 [nXv, nYv] = size(v); % Grabs the size of the rows and columns in v
 x_p = dX/2:dX:L-dX/2;
 y_p = dY/2:dY:L-dY/2;
+I = max([nXu nXv]);
+J = max([nYu, nYv]);
 end
 %% Function to solve for u and v
-function [u, v] = solveUV(u,v,dX,dY,dT,nXu,nYu,nXv,nYv,gamma,nu)
+function [u, v] = solveUV(u,v,dX,dY,dT,I,J,gamma,nu)
 % Part a
-part_a_u = d2u_dx2_plus_d2u_dy2(u,dX,dY,nXu,nYu);
-part_a_v = d2v_dx2_plus_d2v_dy2(v,dX,dY,nXv,nYv);
+part_a_u = d2u_dx2_plus_d2u_dy2(u,dX,dY,I,J);
+part_a_v = d2v_dx2_plus_d2v_dy2(v,dX,dY,I,J);
 % Part b
-part_b_u = du2_dx(u,dX,nXu,nYu,gamma);
-part_b_v = dv2_dy(v,dY,nXv,nYv,gamma);
+part_b_u = du2_dx(u,dX,I,J,gamma);
+part_b_v = dv2_dy(v,dY,I,J,gamma);
 % Part c
-part_c_u = duv_dy(u,v,dY,nXu,nYu,gamma);
-part_c_v = duv_dx(u,v,dX,nXv,nYv,gamma);
+part_c_u = duv_dy(u,v,dY,I,J,gamma);
+part_c_v = duv_dx(u,v,dX,I,J,gamma);
 % Computing u and v
-u(2:nXu-1, 2:nYu-1) = u(2:nXu-1, 2:nYu-1) + dT*(nu * part_a_u - part_b_u - part_c_u);
-v(2:nXv-1, 2:nYv-1) = v(2:nXv-1, 2:nYv-1) + dT*(nu * part_a_v - part_b_v - part_c_v);
+u(2:I-1, 2:J) = u(2:I-1, 2:J) + dT*(nu * part_a_u - part_b_u - part_c_u);
+v(2:I, 2:J-1) = v(2:I, 2:J-1) + dT*(nu * part_a_v - part_b_v - part_c_v);
 end
 %% Approximate functions for u
-function out = d2u_dx2_plus_d2u_dy2(u,dX,dY,nXu,nYu)
-d2u_dx2 = zeros(nXu-2,nYu-2);
-d2u_dy2 = zeros(nXu-2,nYu-2);
-for j = 2:nYu-1
-    for i = 2:nXu-1
+function out = d2u_dx2_plus_d2u_dy2(u,dX,dY,I,J)
+nX = length(2:I-1);
+nY = length(2:J);
+d2u_dx2 = zeros(nX,nY);
+d2u_dy2 = zeros(nX,nY);
+for j = 2:J
+    for i = 2:I-1
         d2u_dx2(i-1,j-1) = (u(i+1,j) - 2*u(i,j) + u(i-1,j))/dX^2;
         d2u_dy2(i-1,j-1) = (u(i,j+1) - 2*u(i,j) + u(i,j-1))/dY^2;
     end
 end
 out = d2u_dx2 + d2u_dy2;
 end
-function out = du2_dx(u,dX,nXu,nYu,gamma)
-out = zeros(nXu-2,nYu-2);
-for j = 2:nYu-1
-    for i = 2:nXu-1
+function out = du2_dx(u,dX,I,J,gamma)
+nX = length(2:I-1);
+nY = length(2:J);
+out = zeros(nX,nY);
+for j = 2:J
+    for i = 2:I-1
         out(i-1,j-1) = (1/dX)...
             *(((u(i,j) + u(i+1,j))/2)^2 ...
             -(((u(i-1,j) + u(i,j))/2)^2)) ...
@@ -153,10 +165,12 @@ for j = 2:nYu-1
     end
 end
 end
-function out = duv_dy(u,v,dY,nXu,nYu,gamma)
-out = zeros(nXu-2,nYu-2);
-for j = 2:nYu-1
-    for i = 2:nXu-1
+function out = duv_dy(u,v,dY,I,J,gamma)
+nX = length(2:I-1);
+nY = length(2:J);
+out = zeros(nX,nY);
+for j = 2:J
+    for i = 2:I-1
         out(i-1,j-1) = (1/dY)...
             *(((v(i,j)+v(i+1,j))/2)...
             *((u(i,j+1)+u(i,j))/2)...
@@ -171,21 +185,25 @@ for j = 2:nYu-1
 end
 end
 %% Approximate functions for v
-function out = d2v_dx2_plus_d2v_dy2(v,dX,dY,nXv,nYv)
-d2v_dx2 = zeros(nXv-2,nYv-2);
-d2v_dy2 = zeros(nXv-2,nYv-2);
-for j = 2:nYv-1
-    for i = 2:nXv-1
+function out = d2v_dx2_plus_d2v_dy2(v,dX,dY,I,J)
+nX = length(2:I);
+nY = length(2:J-1);
+d2v_dx2 = zeros(nX,nY);
+d2v_dy2 = zeros(nX,nY);
+for j = 2:J-1
+    for i = 2:I
         d2v_dx2(i-1,j-1) = (v(i+1,j) - 2*v(i,j) + v(i-1,j))/dX^2;
         d2v_dy2(i-1,j-1) = (v(i,j+1) - 2*v(i,j) + v(i,j-1))/dY^2;
     end
 end
 out = d2v_dx2 + d2v_dy2;
 end
-function out = dv2_dy(v,dY,nXv,nYv,gamma)
-out = zeros(nXv-2,nYv-2);
-for j = 2:nYv-1
-    for i = 2:nXv-1
+function out = dv2_dy(v,dY,I,J,gamma)
+nX = length(2:I);
+nY = length(2:J-1);
+out = zeros(nX,nY);
+for j = 2:J-1
+    for i = 2:I
         out(i-1,j-1) = (1/dY)...
             *(((v(i,j) + v(i,j+1))/2)^2 ...
             -(((v(i,j-1) + v(i,j))/2)^2)) ...
@@ -197,10 +215,12 @@ for j = 2:nYv-1
     end
 end
 end
-function out = duv_dx(u,v,dX,nXv,nYv,gamma)
-out = zeros(nXv-2,nYv-2);
-for j = 2:nYv-1
-    for i = 2:nXv-1
+function out = duv_dx(u,v,dX,I,J,gamma)
+nX = length(2:I);
+nY = length(2:J-1);
+out = zeros(nX,nY);
+for j = 2:J-1
+    for i = 2:I
         out(i-1,j-1) = (1/dX)...
             *(((v(i+1,j)+v(i,j))/2)...
             *((u(i,j+1)+u(i,j))/2)...
