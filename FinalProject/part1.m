e96282c256049a488c7812fcfb23076abd9ccc82
Nathan Flynn <nflynn4@wisc.edu>
2022-12-09 11:00:23 -0600

    Double checked part1. Added graphing stuff as well

--- FinalProject/part1.m
+++ FinalProject/part1.m
@@ -3,80 +3,140 @@
 % ME 573 - Final Project
 clc; close all; clear
 format long;
-
+%(i)
+nu = 0.5;
+[x_p,y_p,uCenter,vCenter,umag,time_i,KE_i] = run_sim(nu);
+% Plot 1
+figure('units','normalized','position',[0.3,0.65,0.4,0.4])
+quiver(x_p,y_p,uCenter,vCenter,2)
+set(gca,'fontsize',26)
+xlabel('x_p')
+ylabel('y_p')
+% xlim([-0.125 1.125])
+% ylim([-0.125 1.125])
+[~,b] = title('Velocity Field \nu=0.5','\Deltax=0.05, \Deltay=0.05, \gamma=0.5, C=0.005, Tfinal=1');
+b.FontSize = 16;
+% Plot 2
+figure('units','normalized','position',[0.3,0,0.4,0.4])
+contour(x_p,y_p,umag,'ShowText','on')
+set(gca,'fontsize',26)
+xlabel('x_p')
+ylabel('y_p')
+% xlim([-0.125 1.125])
+% ylim([-0.125 1.125])
+[~,b] = title('Velocity Magnitude \nu=0.5','\Deltax=0.05, \Deltay=0.05, \gamma=0.5, C=0.005, Tfinal=1');
+b.FontSize = 16;
+%(ii)
+nu = 0.05;
+[x_p,y_p,uCenter,vCenter,umag,time_ii,KE_ii] = run_sim(nu);
+% Plot 1
+figure('units','normalized','position',[0.3,0.65,0.4,0.4])
+quiver(x_p,y_p,uCenter,vCenter,2)
+set(gca,'fontsize',26)
+xlabel('x_p')
+ylabel('y_p')
+% xlim([-0.125 1.125])
+% ylim([-0.125 1.125])
+[~,b] = title('Velocity Field \nu=0.05','\Deltax=0.05, \Deltay=0.05, \gamma=0.5, C=0.005, Tfinal=1');
+b.FontSize = 16;
+% Plot 2
+figure('units','normalized','position',[0.3,0,0.4,0.4])
+contour(x_p,y_p,umag,'ShowText','on')
+set(gca,'fontsize',26)
+xlabel('x_p')
+ylabel('y_p')
+% xlim([-0.125 1.125])
+% ylim([-0.125 1.125])
+[~,b] = title('Velocity Magnitude \nu=0.05','\Deltax=0.05, \Deltay=0.05, \gamma=0.5, C=0.005, Tfinal=1');
+b.FontSize = 16;
+%(iii)
+nu = 0.005;
+[x_p,y_p,uCenter,vCenter,umag,time_iii,KE_iii] = run_sim(nu);
+% Plot 1
+figure('units','normalized','position',[0.3,0.65,0.4,0.4])
+quiver(x_p,y_p,uCenter,vCenter,2)
+set(gca,'fontsize',26)
+xlabel('x_p')
+ylabel('y_p')
+% xlim([-0.125 1.125])
+% ylim([-0.125 1.125])
+[~,b] = title('Velocity Field \nu=0.005','\Deltax=0.05, \Deltay=0.05, \gamma=0.5, C=0.005, Tfinal=1');
+b.FontSize = 16;
+% Plot 2
+figure('units','normalized','position',[0.3,0,0.4,0.4])
+contour(x_p,y_p,umag,'ShowText','on')
+set(gca,'fontsize',26)
+xlabel('x_p')
+ylabel('y_p')
+% xlim([-0.125 1.125])
+% ylim([-0.125 1.125])
+[~,b] = title('Velocity Magnitude \nu=0.005','\Deltax=0.05, \Deltay=0.05, \gamma=0.5, C=0.005, Tfinal=1');
+b.FontSize = 16;
+% Plot 3
+figure('units','normalized','position',[0.65,0.01,0.5,0.5])
+hold on;
+plot(time_i,KE_i,'-')
+plot(time_ii,KE_ii)
+plot(time_iii,KE_iii)
+legend('\nu = 0.5','\nu=0.05','\nu=0.005')
+set(gca,'fontsize',26)
+xlabel('time (s)')
+[~,b] = title('Total Kinetic Energy','\Deltax=0.05, \Deltay=0.05, \gamma=0.5, C=0.005, Tfinal=1');
+b.FontSize = 16;
+%% Function to run the simulation (used to generate multiple plots)
+function [x_p,y_p,uCenter,vCenter,umag,time,KE] = run_sim(nu)
 % Simulation inputs 
 dX = 0.05;
 dY = 0.05; 
 L = 1;
 gamma = 0.5;
-nu = 0.005;
-C = 0.01;
-tFinal  = 1.2;
+C = 0.005;
+tFinal  = 1;
 
 % Calcluated inputs
 dT = C * min(1/4 * dX^2/nu);
 time = 0:dT:tFinal;
 
 % Initializing nodes and applying initial conditions
-[u,v,xu,yu,xv,yv,x_p,y_p,I,J] = generateNodes(dX, dY, L);
+[u,v,~,~,~,~,x_p,y_p,I,J] = generateNodes(dX, dY, L);
 
 % Allocating arrays
 KE = zeros(1, length(time));
-uCenter = zeros(I-1,J-1);
-vCenter = zeros(I-1,J-1);
+uCenter = zeros(I,J);
+vCenter = zeros(I,J);
 
 % Solving u and v
 for i = 1:length(0:dT:tFinal)
     %% Applying boundary conditions to the velocity field
     % Left boundary
-    u(1,:) = 0;
+    u(1,2:J) = 0;
     % Right boundary
-    u(I,:) = 0;
+    u(I,2:J) = 0;
     % Bottom boundary
-    u(:,1) = 0;
+    u(2:I,1) = 0;
     % Top boundary
-    u(:,J+1) = 0;
+    u(2:I,J+1) = 0;
     %% Solving for u and v
     [u, v] = solveUV(u,v,dX,dY,dT,I,J,gamma,nu);
 
     %% Computing the umag and kenetic energy
     % Getting the centers of all of the u nodes
-    for j = 2:J
-        for  k = 2:I
-            uCenter(k-1,j-1) = (u(k-1,j) + u(k,j))/2;
-            vCenter(k-1,j-1) = (v(k,j-1) + v(k,j))/2;
+    for k = 2:I
+        for  j = 2:J+1
+            uCenter(k,j-1) = (u(k,j) + u(k,j-1))/2;
+        end
+    end
+    for k = 2:J+1
+        for j = 2:I
+            vCenter(k-1,j) = (v(k,j) + v(k-1,j))/2;
         end
     end
     % Calculating the velocity magnitude
     umag = sqrt(uCenter.^2 + vCenter.^2);
-    % Calculating the kenetic energy (KE)
-    KE(i) = KE(i) + sum(0.5*dX*dY*(uCenter.^2 + vCenter.^2),"All");
+     % Calculating the kenetic energy (KE)
+    KE(i) = KE(i) + 0.5*dX*dY*sum((uCenter.^2 + vCenter.^2),"All");
 end
-%% Plots-------------------------------------------------------------------
-% Plot 1
-figure('units','normalized','position',[0.3,0.65,0.4,0.4])
-quiver(x_p,y_p,uCenter,vCenter,2)
-set(gca,'fontsize',26)
-xlim([0 1])
-ylim([0 1])
-xlabel('x_p')
-ylabel('y_p')
-title('Velocity Field')
-% Plot 2
-figure('units','normalized','position',[0.3,0,0.4,0.4])
-contour(x_p,y_p,umag,'ShowText','on')
-set(gca,'fontsize',26)
-xlim([0 1])
-ylim([0 1])
-xlabel('x_p')
-ylabel('y_p')
-title('Velocity Magnitude')
-% Plot 3
-figure('units','normalized','position',[0.65,0.01,0.5,0.5])
-plot(time,KE,'-')
-set(gca,'fontsize',26)
-xlabel('time (s)')
-title('Total Kinetic Energy')
+end
 %% Function to generate U nodes
 function [u,v,xu,yu,xv,yv,x_p,y_p,I,J] = generateNodes(dX, dY, L)
 % Nodes in x
@@ -94,12 +154,13 @@ yv = 0:dY:L;
 % Applying initial conditions
 u = ((sin(pi*x_u).^2).*sin(2*pi*y_u));
 v = -((sin(pi*y_v)).^2).*sin(2*pi*x_v);
-u =u';
+u = u';
 v = v';
 
 % Points for pressure 
-x_p = dX/2:dX:L-dX/2;
-y_p = dY/2:dY:L-dY/2;
+x_p = 0:dX:L;
+y_p = 0:dY:L;
+
 % Indicies 
 I = length(xu);
 J = length(yv);
@@ -125,8 +186,8 @@ d2u_dx2 = zeros(I,J+1);
 d2u_dy2 = zeros(I,J+1);
 for j = 2:J
     for i = 2:I-1
-        d2u_dx2(i,j) = (u(i+1,j) - 2*u(i,j) + u(i-1,j))/dX^2;
-        d2u_dy2(i,j) = (u(i,j+1) - 2*u(i,j) + u(i,j-1))/dY^2;
+        d2u_dx2(i,j) = (u(i+1,j) - 2*u(i,j) + u(i-1,j))/dX^2; %Checked
+        d2u_dy2(i,j) = (u(i,j+1) - 2*u(i,j) + u(i,j-1))/dY^2; %Checked
     end
 end
 out = d2u_dx2 + d2u_dy2;
@@ -139,10 +200,10 @@ for j = 2:J
             *(((u(i,j) + u(i+1,j))/2)^2 ...
             -(((u(i-1,j) + u(i,j))/2)^2)) ...
             +(gamma/dX)...
-            *(abs(u(i,j)+u(i+1,j))/2 ...
-            *(u(i,j)-u(i+1,j))/2 ...
-            -abs(u(i-1,j)+u(i,j))/2 ...
-            *(u(i-1,j)-u(i,j))/2);
+            *((abs(u(i,j)+u(i+1,j))/2) ...
+            *((u(i,j)-u(i+1,j))/2) ...
+            -(abs(u(i-1,j)+u(i,j))/2) ...
+            *((u(i-1,j)-u(i,j))/2)); %Checked
     end
 end
 end
@@ -156,10 +217,10 @@ for j = 2:J
             -((v(i,j-1)+v(i+1,j-1))/2)...
             *((u(i,j-1)+u(i,j))/2))...
             +(gamma/dY)...
-            *(abs((v(i,j) + v(i+1,j)))/2 ...
-            *(u(i,j) - u(i,j+1))/2 ...
-            -(abs((v(i,j-1) + v(i+1,j-1)))/2) ...
-            *(u(i,j-1) - u(i,j))/2);
+            *(abs((v(i,j) + v(i+1,j))/2) ...
+            *((u(i,j) - u(i,j+1))/2) ...
+            -(abs((v(i,j-1) + v(i+1,j-1))/2)) ...
+            *((u(i,j-1) - u(i,j))/2)); %Checked
      end
 end
 end
@@ -169,8 +230,8 @@ d2v_dx2 = zeros(I+1,J);
 d2v_dy2 = zeros(I+1,J);
 for j = 2:J-1
     for i = 2:I
-        d2v_dx2(i,j) = (v(i+1,j) - 2*v(i,j) + v(i-1,j))/dX^2;
-        d2v_dy2(i,j) = (v(i,j+1) - 2*v(i,j) + v(i,j-1))/dY^2;
+        d2v_dx2(i,j) = (v(i+1,j) - 2*v(i,j) + v(i-1,j))/dX^2; %Checked
+        d2v_dy2(i,j) = (v(i,j+1) - 2*v(i,j) + v(i,j-1))/dY^2; %Checked
     end
 end
 out = d2v_dx2 + d2v_dy2;
@@ -181,12 +242,12 @@ for j = 2:J-1
     for i = 2:I
         out(i,j) = (1/dY)...
             *(((v(i,j) + v(i,j+1))/2)^2 ...
-            -(((v(i,j-1) + v(i,j))/2)^2)) ...
+            -((v(i,j-1) + v(i,j))/2)^2) ...
             +(gamma/dY)...
-            *(abs(v(i,j)+v(i,j+1))/2 ...
-            *(v(i,j)-v(i,j+1))/2 ...
-            -abs(v(i,j-1)+v(i,j))/2 ...
-            *(v(i,j-1)-v(i,j))/2);
+            *((abs(v(i,j)+v(i,j+1))/2) ...
+            *((v(i,j)-v(i,j+1))/2) ...
+            -(abs(v(i,j-1)+v(i,j))/2) ...
+            *((v(i,j-1)-v(i,j))/2)); %Checked
     end
 end
 end
@@ -200,10 +261,10 @@ for j = 2:J-1
             -((v(i-1,j)+v(i,j))/2)...
             *((u(i-1,j+1)+u(i-1,j))/2))...
             +(gamma/dX)...
-            *(abs((u(i,j+1)+u(i,j)))/2 ...
-            *(v(i,j) - v(i+1,j))/2 ...
-            -(abs((u(i-1,j+1) + u(i-1,j)))/2) ...
-            *(v(i-1,j) - v(i,j))/2);
+            *((abs((u(i,j+1)+u(i,j))/2)) ...
+            *((v(i,j) - v(i+1,j))/2) ...
+            -(abs((u(i-1,j+1) + u(i-1,j))/2)) ...
+            *((v(i-1,j) - v(i,j))/2)); %Checked
      end
 end
 end
